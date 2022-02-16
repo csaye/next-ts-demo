038fb6efaac9e0c10066c4f13fcc9d3e638fe5f5
Cooper Saye <coopersaye@gmail.com>
2022-02-16 11:14:22 -0800

    Clean up files.

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,8 +1,20 @@
-import '../styles/globals.css'
-import type { AppProps } from 'next/app'
+import Head from 'next/head';
 
-function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+import type { AppProps } from 'next/app';
+
+import '../styles/globals.css';
+
+export default function App(props: AppProps) {
+  const { Component, pageProps } = props;
+
+  return (
+    <>
+      <Head>
+        <title>Next TS Demo</title>
+        <meta name="description" content="Demo of TypeScript used with Next.js." />
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+      <Component {...pageProps} />
+    </>
+  );
 }
-
-export default MyApp
