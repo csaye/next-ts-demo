9661f87d64c20cc86dda153b3428f1e193725138
Cooper Saye <coopersaye@gmail.com>
2022-02-17 23:47:39 -0800

    Listen for keys.

--- components/Page.tsx
+++ components/Page.tsx
@@ -1,3 +1,5 @@
+import { useEffect } from 'react';
+
 import styles from '../styles/components/Page.module.css';
 
 type Props = {
@@ -5,6 +7,17 @@ type Props = {
 };
 
 export default function Page(props: Props) {
+  // called on key press
+  function onKeydown(e: KeyboardEvent) {
+    console.log(e.key);
+  }
+
+  // listen for keys on start
+  useEffect(() => {
+    window.addEventListener('keydown', onKeydown);
+    return () => window.removeEventListener('keydown', onKeydown);
+  }, []);
+
   return (
     <div>
       <h1>{props.text}</h1>
