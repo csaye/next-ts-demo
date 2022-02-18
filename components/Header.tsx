b9264cc0df4d24f92405c670db724e83055aafe6
Cooper Saye <coopersaye@gmail.com>
2022-02-17 23:59:14 -0800

    Add header links.

--- components/Header.tsx
+++ components/Header.tsx
@@ -1,8 +1,16 @@
+import Link from 'next/link';
+
 import styles from '../styles/components/Header.module.css';
 
 export default function Header() {
   return (
     <div>
+      <Link href="/">
+        <a>Home</a>
+      </Link>
+      <Link href="/about">
+        <a>About</a>
+      </Link>
     </div>
   );
 }
