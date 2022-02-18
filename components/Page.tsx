79b7c428b93e67e48cbbddef16fc20a8ec00f6cc
Cooper Saye <coopersaye@gmail.com>
2022-02-17 23:48:43 -0800

    Append keys to text.

--- components/Page.tsx
+++ components/Page.tsx
@@ -1,4 +1,4 @@
-import { useEffect } from 'react';
+import { useEffect, useState } from 'react';
 
 import styles from '../styles/components/Page.module.css';
 
@@ -7,20 +7,22 @@ type Props = {
 };
 
 export default function Page(props: Props) {
+  const [text, setText] = useState(props.text);
+
   // called on key press
   function onKeydown(e: KeyboardEvent) {
-    console.log(e.key);
+    setText(val => val + e.key);
   }
 
   // listen for keys on start
   useEffect(() => {
     window.addEventListener('keydown', onKeydown);
     return () => window.removeEventListener('keydown', onKeydown);
-  }, []);
+  }, [onKeydown]);
 
   return (
     <div>
-      <h1>{props.text}</h1>
+      <h1>{text}</h1>
     </div>
   );
 }
