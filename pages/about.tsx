942534e7ae049ef0c65f03b36263ff6dc9515219
Cooper Saye <coopersaye@gmail.com>
2022-02-17 23:51:05 -0800

    Create about page.

---
+++ pages/about.tsx
@@ -0,0 +1,12 @@
+import type { NextPage } from 'next';
+
+import styles from '../styles/pages/About.module.css';
+
+const About: NextPage = () => {
+  return (
+    <div>
+    </div>
+  );
+}
+
+export default About;
